2ab3016709709cdb44dc66dfb9f487426bd36088
rigGitLikeYouDigGit <arthur.edward76@googlemail.com>
2023-09-08 22:03:35 +0100

    fixed basic single selection - we take those

--- python/wptree/ui/view.py
+++ python/wptree/ui/view.py
@@ -56,6 +56,7 @@ class TreeView(QtWidgets.QTreeView):
 		self.makeBehaviour()
 		self.makeAppearance()
 		self.setSelectionBehavior(self.SelectRows)
+		#self.setSelectionMode(self.ExtendedSelection)
 		self.setEditTriggers(self.DoubleClicked)
 
 
@@ -195,7 +196,27 @@ class TreeView(QtWidgets.QTreeView):
 	def mouseDoubleClickEvent(self, event:PySide2.QtGui.QMouseEvent) -> None:
 		"""manage key state"""
 		#self.keyState.mouseDoubleClicked(event)
+		if event.button() == QtCore.Qt.RightButton: #double right click get out of here
+			return
+			return super(TreeView, self).mouseDoubleClickEvent(event)
 		super(TreeView, self).mouseDoubleClickEvent(event)
+
+	def mouseReleaseEvent(self, event:PySide2.QtGui.QMouseEvent) -> None:
+		"""manage key state"""
+		self.keyState.mouseReleased(event)
+		#super(TreeView, self).mouseReleaseEvent(event)
+
+	def dragEnterEvent(self, event:PySide2.QtGui.QDragEnterEvent) -> None:
+		return
+
+	def dragMoveEvent(self, event:PySide2.QtGui.QDragMoveEvent) -> None:
+		return
+
+	def dragLeaveEvent(self, event:PySide2.QtGui.QDragLeaveEvent) -> None:
+		return
+
+	def mouseMoveEvent(self, event:PySide2.QtGui.QMouseEvent) -> None:
+		return
 
 	def mousePressEvent(self, event):
 		"""manage key state"""
@@ -214,38 +235,72 @@ class TreeView(QtWidgets.QTreeView):
 			# clicked on expand icon, not actual item
 			return super(TreeView, self).mousePressEvent(event)
 
+		#return super(TreeView, self).mousePressEvent(event)
 
 		self.onClicked(index)
-		event.accept()
+		self.ensureRowsSelected()
+		#event.accept()
+		print("selected", self.selectionModel().selectedIndexes())
+
 		return
 		# use temporary selection model to avoid super() messing with it
 		mainSelModel = self.selectionModel()
 		sel = mainSelModel.selection()
 		self.setSelectionModel(QtCore.QItemSelectionModel())
-		super(TreeView, self).mousePressEvent(event)
-
+		#super(TreeView, self).mousePressEvent(event)
 		mainSelModel.clear()
 		self.setSelectionModel(mainSelModel)
 		mainSelModel.select(sel, QtCore.QItemSelectionModel.Select)
 
+	def ensureRowsSelected(self):
+		"""run after selection change to ensure rows are selected"""
+
+		# get full rows from selected row indices (output from OnClicked)
+		selIndices = []
+		indices = self.selectionModel().selectedIndexes()
+		for i in indices:
+			if i.column() == 0:
+				selIndices.append(i)
+		self.selectionModel().clearSelection()
+		for rowIndex in selIndices:
+			for itemIndex in self.indicesForRowIndex(rowIndex):
+				self.selectionModel().select(itemIndex, QtCore.QItemSelectionModel.Select)
 
 
-	def onClicked(self, index):
-		""" manage selection manually """
+	def onClicked(self, index:QtCore.QModelIndex):
+		""" manage selection manually
+		selection only ever works by rows"""
+
+		#rowIndices = self.indicesForRowIndex(index)
+		#rowSel = QtCore.QItemSelection()
+		# for row in rowIndices:
+		# 	rowSel.select(row, row)
 
 		index = self.model().index(index.row(), 0, index.parent())
 
 		if not self.keyState.shift:
 			# no contiguous selection
-			command = QtCore.QItemSelectionModel.ClearAndSelect
+
+			#print("key state", self.keyState.ctrl, self.keyState.debug())
+
 			if self.keyState.ctrl:
 				command = QtCore.QItemSelectionModel.Toggle
-			if self.keyState.alt:
+			elif self.keyState.alt:
+				print("deselect")
 				command = QtCore.QItemSelectionModel.Deselect
+			else:
+				#self.selectionModel().clear()
+				command = QtCore.QItemSelectionModel.ClearAndSelect
+
 			self.selectionModel().select(
 				index,
 				command
 			)
+			self.selectionModel().setCurrentIndex(
+				index, QtCore.QItemSelectionModel.Current
+			)
+			return
+
 		elif self.keyState.shift: # contiguous span
 
 			clickRow = self.model().rowFromIndex(index)
@@ -326,6 +381,13 @@ class TreeView(QtWidgets.QTreeView):
 				continue
 			branchList.append(branch)
 		return branchList
+
+	def indicesForRowIndex(self, index:QtCore.QModelIndex)->T.List[QtCore.QModelIndex]:
+		"""returns all indices in the same row as the given index"""
+		indices = []
+		for i in range(self.model().columnCount()):
+			indices.append(self.model().index(index.row(), i, index.parent()))
+		return indices
 
 	def selectedRowIndices(self)->list[QtCore.QModelIndex]:
 		"""returns branch indices for all name and value items selected in ui"""
