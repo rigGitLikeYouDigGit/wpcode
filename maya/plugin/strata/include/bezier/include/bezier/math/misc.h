71bee88b142c9fc132b2fcbf704b74f74929ac47
rigGitLikeYouDigGit <41040965+rigGitLikeYouDigGit@users.noreply.github.com>
2026-03-28 02:46:11 +0000

    moving c include files around

--- maya/plugin/strata/include/bezier/include/bezier/math/misc.h
+++
@@ -1,8 +0,0 @@
-#ifndef BEZIER_MISC_H
-#define BEZIER_MISC_H
-
-namespace bezier {
-    int factorial(int n);
-}
-
-#endif //BEZIER_MISC_H
--- maya/plugin/strata/src/bezier/math/misc.h
+++
@@ -1,8 +0,0 @@
-#ifndef BEZIER_MISC_H
-#define BEZIER_MISC_H
-
-namespace bez {
-    int factorial(int n);
-}
-
-#endif //BEZIER_MISC_H
